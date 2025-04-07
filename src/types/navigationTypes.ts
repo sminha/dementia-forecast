b355df8c8493fd6ba5d89ea48dd0b25afbafc886
sminha <sminha8590@naver.com>
2025-04-07 17:55:02 +0900

    feat: Add navigation type RootStackParamList

---
+++ src/types/navigationTypes.ts
@@ -0,0 +1,9 @@
+export type RootStackParamList = {
+  Splash: undefined;
+  Home: undefined;
+  Login: undefined;
+  EmailSignUp: undefined;
+  LifestyleStart: undefined;
+  LifestyleQuestion: undefined;
+  LifestyleComplete: undefined;
+};
