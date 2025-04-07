2738100950d9315d4a9f4d3d6807e9301f369e6c
sminha <sminha8590@naver.com>
2025-04-07 17:52:08 +0900

    feat: Add navigation for Auth and Lifestyle screens

--- App.tsx
+++ App.tsx
@@ -3,6 +3,11 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import SplashScreen from './src/screens/SplashScreen.tsx';
 import HomeScreen from './src/screens/main/HomeScreen.tsx';
+import LoginScreen from './src/screens/auth/LoginScreen.tsx';
+import EmailSignUpScreen from './src/screens/auth/EmailSignUpScreen.tsx';
+import LifestyleStartScreen from './src/screens/lifestyle/LifestyleStartScreen.tsx';
+import LifestyleQuestionScreen from './src/screens/lifestyle/LifestyleQuestionScreen.tsx';
+import LifestyleCompleteScreen from './src/screens/lifestyle/LifestyleCompleteScreen.tsx';
 
 const Stack = createStackNavigator();
 
@@ -18,6 +23,33 @@ function App(): React.JSX.Element {
         <Stack.Screen
           name="Home"
           component={HomeScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="EmailSignUp"
+          component={EmailSignUpScreen}
+          options={{ headerShown: false }}
+        />
+
+        <Stack.Screen
+          name="LifestyleStart"
+          component={LifestyleStartScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="LifestyleQuestion"
+          component={LifestyleQuestionScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="LifestyleComplete"
+          component={LifestyleCompleteScreen}
+          options={{ headerShown: false }}
         />
       </Stack.Navigator>
     </NavigationContainer>
