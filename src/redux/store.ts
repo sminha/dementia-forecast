3bc7e1f975c05227725e050a39a57cb16e912c28
sminha <sminha8590@naver.com>
2025-04-11 23:58:25 +0900

    feat: Add Redux store

---
+++ src/redux/store.ts
@@ -0,0 +1,11 @@
+import { configureStore } from '@reduxjs/toolkit';
+import signupReducer from './slices/signupSlice.ts';
+
+export const store = configureStore({
+  reducer: {
+    signup: signupReducer,
+  },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
