1133960eb03247d2279a6c9de9c5289d67088252
sminha <sminha8590@naver.com>
2025-05-11 04:09:46 +0900

    feat: Add BiometricCompleteScreen

---
+++ src/screens/biometric/BiometricCompleteScreen.tsx
@@ -0,0 +1,58 @@
+import React, { useEffect } from 'react';
+import { View, StyleSheet } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { StackNavigationProp } from '@react-navigation/stack';
+import { RootStackParamList } from '../../types/navigationTypes.ts';
+import FastImage from 'react-native-fast-image';
+import CustomText from '../../components/CustomText.tsx';
+
+const BiometricCompleteScreen = () => {
+  type Navigation = StackNavigationProp<RootStackParamList, 'BiometricComplete'>;
+  const navigation = useNavigation<Navigation>();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      navigation.navigate('BiometricOverview');
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, [navigation]);
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.textContainer}>
+        <CustomText style={styles.title}>생체정보 입력을 완료했어요!</CustomText>
+      </View>
+
+      <View style={styles.imageContainer}>
+        <FastImage source={require('../../assets/images/complete.gif')} style={styles.image} />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FFFFFF',
+  },
+  textContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+  },
+  title: {
+    fontSize: 24,
+  },
+  imageContainer: {
+    flex: 2,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  image: {
+    width: 130,
+    height: 130,
+  },
+});
+
+export default BiometricCompleteScreen;
