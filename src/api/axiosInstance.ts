48046254240ccccb77715c800a0a5886871961b1
mina <105765658+sminha@users.noreply.github.com>
2025-06-18 16:53:14 +0900

    Delete src directory

--- src/api/axiosInstance.ts
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-import { BACKEND_URL } from '@env';
-
-const axiosInstance = axios.create({
-  baseURL: BACKEND_URL,
-});
-
-export default axiosInstance;
--- src/utils/permissionHelper.ts
+++
@@ -1,9 +0,0 @@
-import { NativeModules, Platform } from 'react-native';
-
-const { ExternalStoragePermission } = NativeModules;
-
-export const openExternalStoragePermission = () => {
-  if (Platform.OS === 'android' && Platform.Version >= 30) {
-    ExternalStoragePermission.openManageAllFilesPermission();
-  }
-};
